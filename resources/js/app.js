db5b19edceaa52e7947a4d260054c2d4c3b70ffb
Abegail Isidro <chabbyabe@gmail.com>
2023-08-20 16:56:28 +0900

    Upadte custom scripts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,6 @@
 import './bootstrap';
 
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
-import customPlugin from './plugins/custom-plugin'
  
-Alpine.plugin(customPlugin)
  
 Livewire.start()
