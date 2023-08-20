fdae3a7c55c8963d1aa4e945d3f0234422e72fac
Abegail Isidro <chabbyabe@gmail.com>
2023-08-20 16:52:46 +0900

    Add alpine and livewire in custom js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import customPlugin from './plugins/custom-plugin'
+ 
+Alpine.plugin(customPlugin)
+ 
+Livewire.start()
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,11 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
-        @livewireStyles()
 
     </head>
     <body>
         {{ $slot }}
-        @livewireScripts()
+        @livewireScriptConfig 
+
     </body>
+
 </html>
